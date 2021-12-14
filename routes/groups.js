0ec4d62eb728120852e7f86c60c48ed2326ee8bd
abejotav1 <abejotav1@hotmail.com>
2021-12-13 18:27:16 -0600

    13-12-2021
    13-12-2021

--- routes/groups.js
+++ routes/groups.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const multer =require('multer');
+const path = require('path');
 const controller = require('../controllers/groupsController');
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null, path.join(__dirname,'../public/img/groups'))
+    },
+    filename:(req,file,cb)=>{
+        console.log(file);
+        const newFilename = 'group-'+Date.now()+path.extname(file.originalname);
+        cb(null, newFilename);
+    }
+})
+
+const upload = multer({storage})
 
 // Todos los grupos
 router.get('/', controller.index);
@@ -10,7 +24,7 @@ router.get('/', controller.index);
 router.get('/create', controller.create);
 
 // Procesamiento del formulario de creación
-router.post('/', controller.store);
+router.post('/', upload.single('group-image'), controller.store);
 
 // Detalle de un grupo
 router.get('/:id', controller.show);
